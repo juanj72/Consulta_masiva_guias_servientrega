44b0295143d37a5026b55390d8e5e5a4a4be5900
juanj72 <juan.jara@campusucc.edu.co>
2023-02-08 00:18:03 -0500

    codigo para consulta de guias con reporte detallado a excel

---
+++ scraping.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from pandas import json_normalize
+
+
+url='https://mobile.servientrega.com/GreenMobile/CO/mapas/DistribucionCliente/getGuiasDistribucionCliente'
+
+
+base=pd.read_excel('base.xlsx')
+reporte=[]
+
+for i in range(len(base)):
+    guia=base['guia'][i]
+
+    datos={
+    "strIdGuia":str(guia)
+    
+
+    }
+    respuesta = requests.post(url, json=datos)
+        # Ahora decodificamos la respuesta como json
+    como_json = respuesta.json()
+
+
+    print("La respuesta del servidor es:")
+    #print(como_json)
+    #data=json_normalize(como_json)
+    reporte.append(como_json)
+    
+    
+
+
+print(reporte)
+reporte=pd.DataFrame(reporte)
+reporte.to_excel('datos.xlsx')
+
+
+
+
